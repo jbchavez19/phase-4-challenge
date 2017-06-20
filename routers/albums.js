30d8e6cba1a55ca7628d9b4dbf2340370d8b7e97
Jose Chavez Dominguez <jbchavez@Joses-MacBook-Pro.local>
2017-06-20 12:09:42 -0700

    Add new review page

--- routers/albums.js
+++ routers/albums.js
@@ -3,6 +3,21 @@ const database = require('../database')
 
 const router = express.Router()
 
+router.get('/:albumID/review', (request, response) => {
+  const albumID = request.params.albumID
+  const albumTitle = request.query.title
+
+  response.render('new_review', {
+    windowTitle: 'New Review',
+    album: { title: albumTitle },
+    isLoggedIn: request.isLoggedIn
+  })
+})
+
+router.post('/:albumID/review', (request, response, next) => {
+  response.send(request.body)
+})
+
 router.get('/:albumID', (request, response, next) => {
   const albumID = request.params.albumID
 
